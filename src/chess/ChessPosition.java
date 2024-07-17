bc79ac9b8f33558bbb32e53a699cc88746fa3753
kittywhite568 <kittywhite568@gmail.com>
2024-07-17 19:54:29 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,39 +3,34 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-	
-	private int row;
+
 	private char column;
-	
-	public ChessPosition(int row, char column) {
+	private int row;
+
+	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
+			
+		throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+			this.row = row;
 		}
 		
-		this.row = row;
-		this.column = column;
-	}
+		public char getColumn() {
+			return column;
+		}
+		
+		public int getRow() {
+			return row;
+		}
 
-	public int getRow() {
-		return row;
-	}
+		protected Position toPosition() {
+			return new Position(8 - row, column - 'a');
+		}
 
-	public char getColumn() {
-		return column;
-	}
-
-	protected Position toPosition() {
-		return new Position(8 - row, column - 'a');
-	}
-	
-	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition(8 - position.getRow(),(char) ('a' - position.getColumn()));
-	}
-	
-	@Override 
-	public String toString() {
-		return "" + column + row;
-	}
-	
-
+		protected static ChessPosition fromPosition(Position position) {
+			return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+			
+		}
 }
+	
