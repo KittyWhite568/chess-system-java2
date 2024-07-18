b3f2d822886385a1ee0700d5a23ea40253a10532
kittywhite568 <kittywhite568@gmail.com>
2024-07-18 13:39:32 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -26,9 +26,16 @@ public class Position {
 	public void setColumn(Integer column) {
 		this.column = column;
 	}
+
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 	
+	@Override
 	public String toString() {
 		return row + ", " + column;
 	}
 
+	
 }
